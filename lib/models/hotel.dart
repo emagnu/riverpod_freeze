eca8a3bcdcbc0557e2bce787bac5eea5b78bc711
Emmanuele <emagnubcp@gmail.com>
2024-01-31 19:11:57 +0000

    11. Freezed - with json_serializable

---
+++ lib/models/hotel.dart
@@ -0,0 +1,37 @@
+//   //
+//  Import LIBRARIES
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+//  Import FILES
+//  PARTS
+//  FREEZE
+part 'hotel.freezed.dart';
+part 'hotel.g.dart';
+//  //
+//  //  //
+//  PROVIDERS
+//  //  //
+
+@freezed
+class Hotel with _$Hotel {
+  @JsonSerializable(explicitToJson: true)
+  const factory Hotel({
+    required String name,
+    required int classification,
+    required String city,
+    @JsonKey(name: 'parking_lot_capacity') int? parkingLotCapacity,
+    @Default([]) List<Review> reviews,
+  }) = _Hotel;
+
+  factory Hotel.fromJson(Map<String, dynamic> json) => _$HotelFromJson(json);
+}
+
+@freezed
+class Review with _$Review {
+  const factory Review({
+    required double score,
+    String? review,
+  }) = _Review;
+
+  factory Review.fromJson(Map<String, Object?> json) => _$ReviewFromJson(json);
+}
